cd61bd08dbc4c596b7dbae0d73392454ca8820fb
Julien Salazar <salazar118@yahoo.com>
2022-05-04 17:52:05 -0700

    initial commit

---
+++ fundamentals/fundamentals/for_loop_basic1/for_loop_basic1.py
@@ -0,0 +1,23 @@
+for x in range (0, 151):
+    print(x)
+
+for x in range (5, 1000, 5):
+    print(x)
+
+for x in range (1, 100):
+    if x%5 == 0:
+        print("Coding")
+    elif x%10 == 0:
+        print("Coding Dojo")
+    else:
+        print(x)
+
+for x in range (2018, 1, -4):
+    print(x)
+
+lowNum = 5
+highNum = 60
+mult = 2
+
+for x in range (lowNum, highNum, mult):
+    print(x)
